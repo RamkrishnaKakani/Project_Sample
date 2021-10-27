2c5b4db65df8ba313b11fe27742564f922b1f372
Ramkrishna Kakani <Ramkrishna.Kakani@allscripts.com>
2021-10-27 14:44:30 +0530

    Comments removed

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,35 +1,35 @@
-# ########################## General Variables ###################################
-# variable "resourceGroup" {
-#   description = "Name of the resource group to be imported."
-#   type        = string
-# }
+########################## General Variables ###################################
+variable "resourceGroup" {
+  description = "Name of the resource group to be imported."
+  type        = string
+}
 
-# variable "location" {
-#   description = "Location"
-#   type        = string
-# }
+variable "location" {
+  description = "Location"
+  type        = string
+}
 
-# variable "tags" {
-#   description = "The tags to associate with your network and subnets."
-#   type        = map(string)
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = map(string)
 
-#   default = {}
-# }
+  default = {}
+}
 
-# ########################## App Service Variables ###############################
-# variable "id" {
-#   description = "ID of the App Service Plan"
-#   type        = string
-# }
+########################## App Service Variables ###############################
+variable "id" {
+  description = "ID of the App Service Plan"
+  type        = string
+}
 
-# variable "apps" {
-#   type = map(object({
-#     app_name   = string
-#     version    = string
-#   }))
-# }
+variable "apps" {
+  type = map(object({
+    app_name   = string
+    version    = string
+  }))
+}
 
-# variable "env" {
-#   description = "Name of Enviroment"
-#   type        = string
-# }
+variable "env" {
+  description = "Name of Enviroment"
+  type        = string
+}
